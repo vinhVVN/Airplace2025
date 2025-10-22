c6bedee21699294ace99b1e4a80f0864ab9ed3bc
Hung Hao Han <lequanghung.work@gmail.com>
2025-10-22 22:13:14 +0700

    Phác thảo cơ bản về giao diện cho chức năng đặt và bán vé máy bay, chưa tối ưu về việc hiển thị trên nhiều kích thước màn hình khác nhau

--- Airplace2025/Program.cs
+++ Airplace2025/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Airplace2025.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Airplace2025
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmDatVe());
         }
     }
 }
