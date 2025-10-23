a178a610d1408c4e5f76ae6cdb484b77db7edc80
Vinh zx <vinh.nguyenthanh26@gmail.com>
2025-10-23 11:54:12 +0700

    Squashed 'Forms/frmDatve/' content from commit f6d33f7
    git-subtree-dir: Forms/frmDatve
    git-subtree-split: f6d33f7760f6cece163661f75af4058da2e5e556

---
+++ Airplace2025/Program.cs
@@ -0,0 +1,23 @@
+﻿using Airplace2025.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Airplace2025
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmDatVe());
+        }
+    }
+}
