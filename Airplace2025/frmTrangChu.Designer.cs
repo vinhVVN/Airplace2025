3c40d5c9c2d7404c55866182c63e7439b6b1d53a
Nguyễn Minh Triết <trietnguyenminh1502@gmail.com>
2025-11-01 00:07:34 +0700

    thêm cửa sổ của trang chủ

---
+++ Airplace2025/frmTrangChu.Designer.cs
@@ -0,0 +1,281 @@
+namespace Airplace2025
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlTopMenu = new Guna.UI2.WinForms.Guna2Panel();
+            this.btnClose = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.btnCaiDat = new Guna.UI2.WinForms.Guna2Button();
+            this.btnTaiKhoanVaQuyen = new Guna.UI2.WinForms.Guna2Button();
+            this.btnMayBay = new Guna.UI2.WinForms.Guna2Button();
+            this.btnBaoCao = new Guna.UI2.WinForms.Guna2Button();
+            this.btnVeMayBay = new Guna.UI2.WinForms.Guna2Button();
+            this.btnChuyenBay = new Guna.UI2.WinForms.Guna2Button();
+            this.picLogo = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.pnlSubMenu = new Guna.UI2.WinForms.Guna2Panel();
+            this.btnChiTietChuyenBay = new Guna.UI2.WinForms.Guna2Button();
+            this.btnTaoChuyenBayMoi = new Guna.UI2.WinForms.Guna2Button();
+            this.btnDanhSachChuyenBay = new Guna.UI2.WinForms.Guna2Button();
+            this.pnlMainContent = new Guna.UI2.WinForms.Guna2Panel();
+            this.pnlTopMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.pnlSubMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTopMenu
+            // 
+            this.pnlTopMenu.BackColor = System.Drawing.Color.White;
+            this.pnlTopMenu.Controls.Add(this.btnClose);
+            this.pnlTopMenu.Controls.Add(this.btnCaiDat);
+            this.pnlTopMenu.Controls.Add(this.btnTaiKhoanVaQuyen);
+            this.pnlTopMenu.Controls.Add(this.btnMayBay);
+            this.pnlTopMenu.Controls.Add(this.btnBaoCao);
+            this.pnlTopMenu.Controls.Add(this.btnVeMayBay);
+            this.pnlTopMenu.Controls.Add(this.btnChuyenBay);
+            this.pnlTopMenu.Controls.Add(this.picLogo);
+            this.pnlTopMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTopMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlTopMenu.Name = "pnlTopMenu";
+            this.pnlTopMenu.Size = new System.Drawing.Size(1200, 50);
+            this.pnlTopMenu.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.FillColor = System.Drawing.Color.Transparent;
+            this.btnClose.HoverState.FillColor = System.Drawing.Color.Red;
+            this.btnClose.HoverState.IconColor = System.Drawing.Color.White;
+            this.btnClose.IconColor = System.Drawing.Color.Black;
+            this.btnClose.Location = new System.Drawing.Point(1150, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(50, 30);
+            this.btnClose.TabIndex = 7;
+            // 
+            // btnCaiDat
+            // 
+            this.btnCaiDat.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnCaiDat.CheckedState.CustomBorderColor = System.Drawing.Color.DodgerBlue;
+            this.btnCaiDat.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnCaiDat.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.btnCaiDat.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnCaiDat.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnCaiDat.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnCaiDat.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnCaiDat.FillColor = System.Drawing.Color.White;
+            this.btnCaiDat.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaiDat.ForeColor = System.Drawing.Color.Black;
+            this.btnCaiDat.Location = new System.Drawing.Point(620, 0);
+            this.btnCaiDat.Name = "btnCaiDat";
+            this.btnCaiDat.Size = new System.Drawing.Size(120, 50);
+            this.btnCaiDat.TabIndex = 6;
+            this.btnCaiDat.Text = "Cài đặt";
+            // 
+            // btnTaiKhoanVaQuyen
+            // 
+            this.btnTaiKhoanVaQuyen.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnTaiKhoanVaQuyen.CheckedState.CustomBorderColor = System.Drawing.Color.DodgerBlue;
+            this.btnTaiKhoanVaQuyen.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnTaiKhoanVaQuyen.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.btnTaiKhoanVaQuyen.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnTaiKhoanVaQuyen.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnTaiKhoanVaQuyen.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnTaiKhoanVaQuyen.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnTaiKhoanVaQuyen.FillColor = System.Drawing.Color.White;
+            this.btnTaiKhoanVaQuyen.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTaiKhoanVaQuyen.ForeColor = System.Drawing.Color.Black;
+            this.btnTaiKhoanVaQuyen.Location = new System.Drawing.Point(490, 0);
+            this.btnTaiKhoanVaQuyen.Name = "btnTaiKhoanVaQuyen";
+            this.btnTaiKhoanVaQuyen.Size = new System.Drawing.Size(130, 50);
+            this.btnTaiKhoanVaQuyen.TabIndex = 5;
+            this.btnTaiKhoanVaQuyen.Text = "Tài khoản và quyền";
+            // 
+            // btnMayBay
+            // 
+            this.btnMayBay.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnMayBay.CheckedState.CustomBorderColor = System.Drawing.Color.DodgerBlue;
+            this.btnMayBay.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnMayBay.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.btnMayBay.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnMayBay.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnMayBay.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnMayBay.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnMayBay.FillColor = System.Drawing.Color.White;
+            this.btnMayBay.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMayBay.ForeColor = System.Drawing.Color.Black;
+            this.btnMayBay.Location = new System.Drawing.Point(400, 0);
+            this.btnMayBay.Name = "btnMayBay";
+            this.btnMayBay.Size = new System.Drawing.Size(90, 50);
+            this.btnMayBay.TabIndex = 4;
+            this.btnMayBay.Text = "Máy bay";
+            // 
+            // btnBaoCao
+            // 
+            this.btnBaoCao.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnBaoCao.CheckedState.CustomBorderColor = System.Drawing.Color.DodgerBlue;
+            this.btnBaoCao.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnBaoCao.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.btnBaoCao.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnBaoCao.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnBaoCao.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnBaoCao.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnBaoCao.FillColor = System.Drawing.Color.White;
+            this.btnBaoCao.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBaoCao.ForeColor = System.Drawing.Color.Black;
+            this.btnBaoCao.Location = new System.Drawing.Point(310, 0);
+            this.btnBaoCao.Name = "btnBaoCao";
+            this.btnBaoCao.Size = new System.Drawing.Size(90, 50);
+            this.btnBaoCao.TabIndex = 3;
+            this.btnBaoCao.Text = "Báo cáo";
+            // 
+            // btnVeMayBay
+            // 
+            this.btnVeMayBay.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnVeMayBay.CheckedState.CustomBorderColor = System.Drawing.Color.DodgerBlue;
+            this.btnVeMayBay.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnVeMayBay.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.btnVeMayBay.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnVeMayBay.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnVeMayBay.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnVeMayBay.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnVeMayBay.FillColor = System.Drawing.Color.White;
+            this.btnVeMayBay.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVeMayBay.ForeColor = System.Drawing.Color.Black;
+            this.btnVeMayBay.Location = new System.Drawing.Point(220, 0);
+            this.btnVeMayBay.Name = "btnVeMayBay";
+            this.btnVeMayBay.Size = new System.Drawing.Size(90, 50);
+            this.btnVeMayBay.TabIndex = 2;
+            this.btnVeMayBay.Text = "Vé máy bay";
+            // 
+            // btnChuyenBay
+            // 
+            this.btnChuyenBay.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnChuyenBay.Checked = true;
+            this.btnChuyenBay.CheckedState.CustomBorderColor = System.Drawing.Color.DodgerBlue;
+            this.btnChuyenBay.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnChuyenBay.CustomBorderThickness = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.btnChuyenBay.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnChuyenBay.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnChuyenBay.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnChuyenBay.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnChuyenBay.FillColor = System.Drawing.Color.White;
+            this.btnChuyenBay.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChuyenBay.ForeColor = System.Drawing.Color.Black;
+            this.btnChuyenBay.Location = new System.Drawing.Point(130, 0);
+            this.btnChuyenBay.Name = "btnChuyenBay";
+            this.btnChuyenBay.Size = new System.Drawing.Size(90, 50);
+            this.btnChuyenBay.TabIndex = 1;
+            this.btnChuyenBay.Text = "Chuyến bay";
+            this.btnChuyenBay.Click += new System.EventHandler(this.btnChuyenBay_Click);
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = ((System.Drawing.Image)(resources.GetObject("picLogo.Image"))); // Logo máy bay/mây
+            this.picLogo.ImageRotate = 0F;
+            this.picLogo.Location = new System.Drawing.Point(10, 5);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(110, 40);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 0;
+            this.picLogo.TabStop = false;
+            // 
+            // pnlSubMenu
+            // 
+            this.pnlSubMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.pnlSubMenu.Controls.Add(this.btnChiTietChuyenBay);
+            this.pnlSubMenu.Controls.Add(this.btnTaoChuyenBayMoi);
+            this.pnlSubMenu.Controls.Add(this.btnDanhSachChuyenBay);
+            this.pnlSubMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlSubMenu.Location = new System.Drawing.Point(0, 50);
+            this.pnlSubMenu.Name = "pnlSubMenu";
+            this.pnlSubMenu.Size = new System.Drawing.Size(1200, 70);
+            this.pnlSubMenu.TabIndex = 1;
+            // 
+            // btnChiTietChuyenBay
+            // 
+            this.btnChiTietChuyenBay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChiTietChuyenBay.BorderRadius = 18;
+            this.btnChiTietChuyenBay.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnChiTietChuyenBay.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnChiTietChuyenBay.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnChiTietChuyenBay.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnChiTietChuyenBay.FillColor = System.Drawing.Color.White;
+            this.btnChiTietChuyenBay.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnChiTietChuyenBay.ForeColor = System.Drawing.Color.Black;
+            this.btnChiTietChuyenBay.Location = new System.Drawing.Point(1050, 15);
+            this.btnChiTietChuyenBay.Name = "btnChiTietChuyenBay";
+            this.btnChiTietChuyenBay.Size = new System.Drawing.Size(130, 40);
+            this.btnChiTietChuyenBay.TabIndex = 2;
+            this.btnChiTietChuyenBay.Text = "Chi tiết chuyến bay";
+            this.btnChiTietChuyenBay.Visible = false; // Mặc định ẩn
+                                                      // 
+
+            // pnlMainContent
+            // 
+            this.pnlMainContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMainContent.Location = new System.Drawing.Point(0, 120);
+            this.pnlMainContent.Name = "pnlMainContent";
+            this.pnlMainContent.Size = new System.Drawing.Size(1200, 680);
+            this.pnlMainContent.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(1200, 800);
+            this.Controls.Add(this.pnlMainContent);
+            this.Controls.Add(this.pnlSubMenu);
+            this.Controls.Add(this.pnlTopMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hệ thống Quản lý Vé máy bay Airplace2025";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.pnlTopMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.pnlSubMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel pnlTopMenu;
+        private Guna.UI2.WinForms.Guna2Panel pnlSubMenu;
+        private Guna.UI2.WinForms.Guna2Panel pnlMainContent;
+        private Guna.UI2.WinForms.Guna2PictureBox picLogo;
+        private Guna.UI2.WinForms.Guna2Button btnChuyenBay;
+        private Guna.UI2.WinForms.Guna2Button btnCaiDat;
+        private Guna.UI2.WinForms.Guna2Button btnTaiKhoanVaQuyen;
+        private Guna.UI2.WinForms.Guna2Button btnMayBay;
+        private Guna.UI2.WinForms.Guna2Button btnBaoCao;
+        private Guna.UI2.WinForms.Guna2Button btnVeMayBay;
+        private Guna.UI2.WinForms.Guna2ControlBox btnClose;
+        private Guna.UI2.WinForms.Guna2Button btnChiTietChuyenBay;
+    }
+}
