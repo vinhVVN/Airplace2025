e14481da00626fa2a84a2a6d619a88600c94da4a
Hung Hao Han <lequanghung.work@gmail.com>
2025-11-02 21:33:34 +0700

    Thêm FlightCard UserControl và tài nguyên liên quan vào dự án: bổ sung FlightCard.cs, FlightCard.Designer.cs và FlightCard.resx.

---
+++ Airplace2025/FlightCard.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Airplace2025
+{
+    partial class FlightCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.leftPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.lblAirline = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblFlightDuration = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblArrivalTerminal = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblArrivalCity = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblArrivalTime = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblNextDay = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblTrangThai = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblDepartureTerminal = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblDepartureCity = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblDepartureTime = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.btnDetails = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.lblClass = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblFrom = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblPrice = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblCurrency = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.leftPanel.SuspendLayout();
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.BorderRadius = 10;
+            this.leftPanel.Controls.Add(this.btnDetails);
+            this.leftPanel.Controls.Add(this.lblAirline);
+            this.leftPanel.Controls.Add(this.lblFlightDuration);
+            this.leftPanel.Controls.Add(this.lblArrivalTerminal);
+            this.leftPanel.Controls.Add(this.lblArrivalCity);
+            this.leftPanel.Controls.Add(this.lblArrivalTime);
+            this.leftPanel.Controls.Add(this.lblNextDay);
+            this.leftPanel.Controls.Add(this.lblTrangThai);
+            this.leftPanel.Controls.Add(this.lblDepartureTerminal);
+            this.leftPanel.Controls.Add(this.lblDepartureCity);
+            this.leftPanel.Controls.Add(this.lblDepartureTime);
+            this.leftPanel.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.leftPanel.Location = new System.Drawing.Point(0, 0);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(483, 140);
+            this.leftPanel.TabIndex = 0;
+            // 
+            // lblAirline
+            // 
+            this.lblAirline.BackColor = System.Drawing.Color.Transparent;
+            this.lblAirline.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAirline.Location = new System.Drawing.Point(260, 45);
+            this.lblAirline.Name = "lblAirline";
+            this.lblAirline.Size = new System.Drawing.Size(216, 17);
+            this.lblAirline.TabIndex = 9;
+            this.lblAirline.Text = "✈ VN 267 Khải thác bởi Vietnam Airlines";
+            // 
+            // lblFlightDuration
+            // 
+            this.lblFlightDuration.BackColor = System.Drawing.Color.Transparent;
+            this.lblFlightDuration.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFlightDuration.Location = new System.Drawing.Point(260, 20);
+            this.lblFlightDuration.Name = "lblFlightDuration";
+            this.lblFlightDuration.Size = new System.Drawing.Size(145, 17);
+            this.lblFlightDuration.TabIndex = 8;
+            this.lblFlightDuration.Text = "⏱ Thời gian bay 2h 10phút";
+            // 
+            // lblArrivalTerminal
+            // 
+            this.lblArrivalTerminal.BackColor = System.Drawing.Color.Transparent;
+            this.lblArrivalTerminal.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArrivalTerminal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(85)))), ((int)(((byte)(114)))));
+            this.lblArrivalTerminal.Location = new System.Drawing.Point(165, 80);
+            this.lblArrivalTerminal.Name = "lblArrivalTerminal";
+            this.lblArrivalTerminal.Size = new System.Drawing.Size(50, 17);
+            this.lblArrivalTerminal.TabIndex = 7;
+            this.lblArrivalTerminal.Text = "Nhà ga 3";
+            // 
+            // lblArrivalCity
+            // 
+            this.lblArrivalCity.BackColor = System.Drawing.Color.Transparent;
+            this.lblArrivalCity.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArrivalCity.Location = new System.Drawing.Point(165, 55);
+            this.lblArrivalCity.Name = "lblArrivalCity";
+            this.lblArrivalCity.Size = new System.Drawing.Size(34, 22);
+            this.lblArrivalCity.TabIndex = 6;
+            this.lblArrivalCity.Text = "SGN";
+            // 
+            // lblArrivalTime
+            // 
+            this.lblArrivalTime.BackColor = System.Drawing.Color.Transparent;
+            this.lblArrivalTime.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArrivalTime.Location = new System.Drawing.Point(165, 20);
+            this.lblArrivalTime.Name = "lblArrivalTime";
+            this.lblArrivalTime.Size = new System.Drawing.Size(66, 34);
+            this.lblArrivalTime.TabIndex = 5;
+            this.lblArrivalTime.Text = "00:10";
+            // 
+            // lblNextDay
+            // 
+            this.lblNextDay.BackColor = System.Drawing.Color.Transparent;
+            this.lblNextDay.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNextDay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(85)))), ((int)(((byte)(114)))));
+            this.lblNextDay.Location = new System.Drawing.Point(176, 5);
+            this.lblNextDay.Name = "lblNextDay";
+            this.lblNextDay.Size = new System.Drawing.Size(45, 15);
+            this.lblNextDay.TabIndex = 4;
+            this.lblNextDay.Text = "+1 ngày";
+            // 
+            // lblTrangThai
+            // 
+            this.lblTrangThai.BackColor = System.Drawing.Color.Transparent;
+            this.lblTrangThai.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTrangThai.Location = new System.Drawing.Point(97, 37);
+            this.lblTrangThai.Name = "lblTrangThai";
+            this.lblTrangThai.Size = new System.Drawing.Size(56, 17);
+            this.lblTrangThai.TabIndex = 3;
+            this.lblTrangThai.Text = "Bay thẳng";
+            // 
+            // lblDepartureTerminal
+            // 
+            this.lblDepartureTerminal.BackColor = System.Drawing.Color.Transparent;
+            this.lblDepartureTerminal.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartureTerminal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(85)))), ((int)(((byte)(114)))));
+            this.lblDepartureTerminal.Location = new System.Drawing.Point(20, 80);
+            this.lblDepartureTerminal.Name = "lblDepartureTerminal";
+            this.lblDepartureTerminal.Size = new System.Drawing.Size(50, 17);
+            this.lblDepartureTerminal.TabIndex = 2;
+            this.lblDepartureTerminal.Text = "Nhà ga 1";
+            // 
+            // lblDepartureCity
+            // 
+            this.lblDepartureCity.BackColor = System.Drawing.Color.Transparent;
+            this.lblDepartureCity.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartureCity.Location = new System.Drawing.Point(20, 55);
+            this.lblDepartureCity.Name = "lblDepartureCity";
+            this.lblDepartureCity.Size = new System.Drawing.Size(37, 22);
+            this.lblDepartureCity.TabIndex = 1;
+            this.lblDepartureCity.Text = "HAN";
+            // 
+            // lblDepartureTime
+            // 
+            this.lblDepartureTime.BackColor = System.Drawing.Color.Transparent;
+            this.lblDepartureTime.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartureTime.Location = new System.Drawing.Point(20, 20);
+            this.lblDepartureTime.Name = "lblDepartureTime";
+            this.lblDepartureTime.Size = new System.Drawing.Size(66, 34);
+            this.lblDepartureTime.TabIndex = 0;
+            this.lblDepartureTime.Text = "22:00";
+            // 
+            // btnDetails
+            // 
+            this.btnDetails.BackColor = System.Drawing.Color.Transparent;
+            this.btnDetails.BorderColor = System.Drawing.Color.LightBlue;
+            this.btnDetails.BorderRadius = 5;
+            this.btnDetails.BorderThickness = 1;
+            this.btnDetails.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDetails.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnDetails.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnDetails.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnDetails.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnDetails.FillColor = System.Drawing.Color.Transparent;
+            this.btnDetails.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnDetails.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(96)))), ((int)(((byte)(125)))));
+            this.btnDetails.Location = new System.Drawing.Point(260, 75);
+            this.btnDetails.Name = "btnDetails";
+            this.btnDetails.Size = new System.Drawing.Size(180, 30);
+            this.btnDetails.TabIndex = 10;
+            this.btnDetails.Text = "Chi tiết hành trình ⓘ";
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BorderRadius = 10;
+            this.guna2Panel1.Controls.Add(this.lblCurrency);
+            this.guna2Panel1.Controls.Add(this.lblPrice);
+            this.guna2Panel1.Controls.Add(this.lblFrom);
+            this.guna2Panel1.Controls.Add(this.lblClass);
+            this.guna2Panel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(110)))));
+            this.guna2Panel1.Location = new System.Drawing.Point(483, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(150, 140);
+            this.guna2Panel1.TabIndex = 1;
+            // 
+            // lblClass
+            // 
+            this.lblClass.BackColor = System.Drawing.Color.Transparent;
+            this.lblClass.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClass.ForeColor = System.Drawing.Color.White;
+            this.lblClass.Location = new System.Drawing.Point(36, 15);
+            this.lblClass.Name = "lblClass";
+            this.lblClass.Size = new System.Drawing.Size(74, 17);
+            this.lblClass.TabIndex = 0;
+            this.lblClass.Text = "PHỔ THÔNG";
+            this.lblClass.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.BackColor = System.Drawing.Color.Transparent;
+            this.lblFrom.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrom.ForeColor = System.Drawing.Color.White;
+            this.lblFrom.Location = new System.Drawing.Point(65, 50);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(14, 15);
+            this.lblFrom.TabIndex = 1;
+            this.lblFrom.Text = "từ";
+            this.lblFrom.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.BackColor = System.Drawing.Color.Transparent;
+            this.lblPrice.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.ForeColor = System.Drawing.Color.White;
+            this.lblPrice.Location = new System.Drawing.Point(32, 68);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(90, 27);
+            this.lblPrice.TabIndex = 2;
+            this.lblPrice.Text = "2.690.000";
+            this.lblPrice.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCurrency
+            // 
+            this.lblCurrency.BackColor = System.Drawing.Color.Transparent;
+            this.lblCurrency.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrency.ForeColor = System.Drawing.Color.White;
+            this.lblCurrency.Location = new System.Drawing.Point(58, 90);
+            this.lblCurrency.Name = "lblCurrency";
+            this.lblCurrency.Size = new System.Drawing.Size(26, 15);
+            this.lblCurrency.TabIndex = 3;
+            this.lblCurrency.Text = "VND";
+            // 
+            // FlightCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.leftPanel);
+            this.Name = "FlightCard";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Size = new System.Drawing.Size(920, 140);
+            this.leftPanel.ResumeLayout(false);
+            this.leftPanel.PerformLayout();
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel leftPanel;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblDepartureTime;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblDepartureCity;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblDepartureTerminal;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblTrangThai;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblNextDay;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblArrivalTime;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblArrivalCity;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblArrivalTerminal;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblFlightDuration;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblAirline;
+        private Guna.UI2.WinForms.Guna2Button btnDetails;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblClass;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblFrom;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblPrice;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblCurrency;
+    }
+}
